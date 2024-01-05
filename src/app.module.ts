e1444587f12408cafb128cd0153a7afebe650fd7
Yuri Cordeiro <yuricordeiroc@hotmail.com>
2024-01-04 21:40:02 -0300

    Add simple architectural project organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CustomerController } from './controllers/customer/customer.controller';
+import { CustomerController } from './controllers/customer.controller';
+import { PaymentController } from './controllers/payment.controller';
+import { PaymentService } from './services/payment.service';
 
 @Module({
   imports: [],
-  controllers: [CustomerController],
-  providers: [],
+  controllers: [CustomerController, PaymentController],
+  providers: [PaymentService],
 })
 export class AppModule {}
